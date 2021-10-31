5015c56fe083d10eb980ef14dfc9c107e9db027e
Lindsay Fabricant <68440117+lfabricant@users.noreply.github.com>
2021-10-31 14:50:13 -0400

    Update Research.m

--- Research.m
+++ Research.m
@@ -15,13 +15,7 @@ angleFinal= input('Enter angle between 0 and 180: ');
 theta = linspace(angleInitial+180,180-angleFinal);        
 
 if angleFinal == 0
-    %x = [0 0];
-    %y = [0 l];
-    %plot(x,y,'b');
-    %disp(['X: ' num2str(x(length(x)))]);
-    %disp(['Y: ' num2str(y(length(y)))]);
-
-    %RIGHT initialize
+    %RIGHT ARM - initialize plot (for straight line)
     xR = [0 l]; 
     yR = [0 0];     
     zR = [l l];
@@ -30,7 +24,7 @@ if angleFinal == 0
     
     hold on
     
-    %LEFT initialize
+    %LEFT ARM - initialize plot (for straight line)
     xL = [0 -l];
     yL = [0 0];       
     zL = [l l];
@@ -39,7 +33,7 @@ if angleFinal == 0
     
     hold on
     
-    %BACK initialize
+    %BACK ARM - initialize plot (for straight line)
     xB = [0 0];
     yB = [0 l];
     zB = [l l];   
@@ -48,7 +42,7 @@ if angleFinal == 0
     
     hold on
     
-    %FRONT initialize
+    %FRONT ARM - initialize plot (for straight line)
     xF = [0 0];
     yF = [0 -l];
     zF = [l l];     
@@ -61,27 +55,27 @@ if angleFinal == 0
     
     plot3(xL(length(xL)),yL(length(yL)),zL(length(zL)), 'ro')
             
-    %RIGHT print
+    %RIGHT ARM - print coordinates of endpoints (for straight line)
     disp(['XR: ' num2str(xR(length(xR)))]);
     disp(['YR: ' num2str(yR(length(yR)))]);
     disp(['ZR: ' num2str(zR(length(zR)))]);
     
-    %LEFT print
+    %LEFT ARM - print coordinates of endpoints (for straight line)
     disp(['XL: ' num2str(xL(length(xL)))]);
     disp(['YL: ' num2str(yL(length(yL)))]);
     disp(['ZL: ' num2str(zL(length(zL)))]);
     
-    %BACK print
+    %BACK ARM - print coordinates of endpoints (for straight line)
     disp(['XB: ' num2str(xB(length(xB)))]);
     disp(['YB: ' num2str(yB(length(yB)))]);
     disp(['ZB: ' num2str(zB(length(zB)))]);
     
-    %FRONT print
+    %FRONT ARM - print coordinates of endpoints (for straight line)
     disp(['XF: ' num2str(xF(length(xF)))]);
     disp(['YF: ' num2str(yF(length(yF)))]);
     disp(['ZF: ' num2str(zF(length(zF)))]);
 else
-    %RIGHT initialize
+    %RIGHT ARM - initialize plot
     xR = radius*sind(theta); 
     yR = radius+radius*cosd(theta);     
     zR = 0*theta;
@@ -90,7 +84,7 @@ else
     
     hold on
     
-    %LEFT initialize
+    %LEFT ARM - initialize plot
     xL = -(radius*sind(theta));
     yL = radius+radius*cosd(theta);       
     zL = 0*theta;
@@ -99,7 +93,7 @@ else
     
     hold on
     
-    %BACK initialize
+    %BACK ARM - initialize plot
     xB = 0*theta;
     yB = radius+radius*cosd(theta);
     zB = radius*sind(theta);     
@@ -108,7 +102,7 @@ else
     
     hold on
     
-    %FRONT initialize
+    %FRONT ARM - initialize plot
     xF = 0*theta;
     yF = radius+radius*cosd(theta);
     zF = radius*sind(theta);     
@@ -124,69 +118,70 @@ else
         angleFinal=k;                                          
         theta = linspace(angleInitial+180,180-angleFinal);        
         
-        %RIGHT update
+        %RIGHT ARM - update coordinates
         xR = (180/k) * (radius*sind(theta));
         yR = 0*theta;
         zR = l + -((180/k) * (radius+radius*cosd(theta)));
     
-        %LEFT update
+        %LEFT ARM - update coordinates
         xL = -((180/k) * (radius*sind(theta)));
         yL = 0*theta;
         zL = l + -((180/k) * (radius+radius*cosd(theta)));
     
-        %BACK update
+        %BACK ARM - update coordinates
         xB = 0*theta;
         yB = -((180/k) * (radius*sind(theta)));
         zB = l + -((180/k) * (radius+radius*cosd(theta)));
     
-        %FRONT update
+        %FRONT ARM - update coordinates
         xF = 0*theta;
         yF = (180/k) * (radius*sind(theta));
         zF = l + -((180/k) * (radius+radius*cosd(theta)));
         
-        %RIGHT plot
+        %RIGHT ARM - set coordinates for plot on the graph
         set(circleR, 'xdata', xR,'ydata', yR, 'zdata', zR);
         set(dotR, 'xdata', xR(length(xR)),'ydata', yR(length(yR)), 'zdata', zR(length(zR)));
         hold on
         drawnow
         
-        %LEFT plot
+        %LEFT ARM - set coordinates for plot on the graph
         set(circleL, 'xdata', xL,'ydata', yL, 'zdata', zL);
         set(dotL, 'xdata', xL(length(xL)),'ydata', yL(length(yL)), 'zdata', zL(length(zL)));
         hold on
         drawnow 
     
-        %BACK plot
+        %BACK ARM - set coordinates for plot on the graph
         set(circleB, 'xdata', xB,'ydata', yB, 'zdata', zB);
         set(dotB, 'xdata', xB(length(xB)),'ydata', yB(length(yB)), 'zdata', zB(length(zB)));
         hold on
         drawnow
     
-        %FRONT plot
+        %FRONT ARM - set coordinates for plot on the graph
         set(circleF, 'xdata', xF,'ydata', yF, 'zdata', zF);
         set(dotF, 'xdata', xF(length(xF)),'ydata', yF(length(yF)), 'zdata', zF(length(zF)));
         drawnow
         
         if k == stop
             hold on
+            % PLOT ALL ARM ENDPOINTS AS RED DOT
             plot3(xL(length(xL)),yL(length(yL)),zL(length(zL)), 'ro')
             
-            %RIGHT print
+            %RIGHT ARM - print final coordinates
             disp(['XR: ' num2str(xR(length(xR)))]);
             disp(['YR: ' num2str(yR(length(yR)))]);
             disp(['ZR: ' num2str(zR(length(zR)))]);
     
-            %LEFT print
+            %LEFT ARM - print final coordinates
             disp(['XL: ' num2str(xL(length(xL)))]);
             disp(['YL: ' num2str(yL(length(yL)))]);
             disp(['ZL: ' num2str(zL(length(zL)))]);
     
-            %BACK print
+            %BACK ARM - print final coordinates
             disp(['XB: ' num2str(xB(length(xB)))]);
             disp(['YB: ' num2str(yB(length(yB)))]);
             disp(['ZB: ' num2str(zB(length(zB)))]);
     
-            %FRONT print
+            %FRONT ARM - print final coordinates
             disp(['XF: ' num2str(xF(length(xF)))]);
             disp(['YF: ' num2str(yF(length(yF)))]);
             disp(['ZF: ' num2str(zF(length(zF)))]);
